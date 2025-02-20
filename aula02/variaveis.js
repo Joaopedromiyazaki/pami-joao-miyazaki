58d554c02c9cf3847479f6b48c8b1b52942da751
João Pedro Miyazaki <miyazakjao120@gmail.com>
2025-02-20 08:28:13 -0300

    Aula sobre tipo de variáveis

---
+++ aula02/variaveis.js
@@ -0,0 +1,20 @@
+var numero = 2;
+let texto = "Hello";
+const oQueEIsso = "?";
+let SKU = ""
+
+console.log('Tipo var:', numero);
+
+numero = 13;
+
+console.log(texto + "o número agora é" + numero);
+console.log(texto , "o número agora é" + numero);
+console.log(`${texto} o número agora é ${numero}`);
+
+texto = 69;
+
+console.log(texto + numero);
+
+console.log(oQueEIsso);
+oQueEIsso = "!"
+console.log(oQueEIsso);
